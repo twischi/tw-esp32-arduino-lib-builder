00d98a3ab2f83c3f0f6b2c71ad3221552e9b8cac
twischi <thomas@wisniewski.info>
2024-06-19 22:31:25 +0200

    changes

--- setMyParameters.sh
+++ setMyParameters.sh
@@ -46,7 +46,7 @@ sS+=" -D info"
 # Build out Folder & post-Build flags
 # ------------------------------------
 #        ~~ NO building  ~   (SKIP_BUILD)   SKIP building for TESTING DEBUGING ONLY
-sS+=" -X"
+#sS+=" -X"
 # OUT    ~~ during build ~~  (AR_OWN_OUT)  to store the build output.
 sS+=" -o $oneUpDir/Out-from_build"
 # Arduino  ~~ post-build ~~  (ESP32_ARDUINO) for use with Arduino.
