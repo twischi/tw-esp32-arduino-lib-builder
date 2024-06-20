42e3ef6451301ba2db5214b1227f5cd26813e1f9
twischi <thomas@wisniewski.info>
2024-06-21 01:21:13 +0200

    new settings

--- setMyParameters.sh
+++ setMyParameters.sh
@@ -20,22 +20,22 @@ echo "Set Parameters for ./build.sh   by this script >> 'setMyParameters.sh'"
 # <arduino-esp32>
 # --------------------------
 # BRANCH                     (AR_BRANCH)   for the building.  
-sS+=" -A idf-release/v5.1"
+#sS+=" -A idf-release/v5.1"
 # COMMIT                     (AR_COMMIT)   for the building.
 #sS+=" -a 2ba3ed3"
 # TAG                        (AR_TAG)      for the building.   
-#sS+=" -g 3.0.1"
+sS+=" -g 3.0.1"
 # FOLDER                     (AR_PATH)     to store it.         
 sS+=" -p $GitHubSources/arduino-esp32"
 # --------------------------
 # <esp-idf>  
 # --------------------------
 # BRANCH                     (IDF_BRANCH)  for the building.   
-sS+=" -I release/v5.1"
+#sS+=" -I release/v5.1"
 # COMMIT                     (IDF_COMMIT)  for the building.   
 #sS+=" -i '<commit-hash>'"
 # TAG                        (IDF_TAG)     for the building.   
-#sS+=" -G v5.1.4"
+sS+=" -G v5.1.4"
 # FOLDER                     (IDF_PATH)    to store it.          
 sS+=" -f $GitHubSources/esp-idf"
 # DEBUG flag                 (BUILD_DEBUG) for compile with idf.py   
@@ -52,8 +52,8 @@ sS+=" -o $oneUpDir/Out-from_build"
 # Arduino  ~~ post-build ~~  (ESP32_ARDUINO) for use with Arduino.
 sS+=" -c $oneUpDir/Arduino/copy/arduino-esp32-$timeStampAR"
 # Arduiono  ~~ post-build ~~ (ARCHIVE_OUT) Set flag to create Arduiono archives
-sS+=" -e"
-# PIO    ~~ post-build ~~    (POI_OUT_F) Set flag to create PIO archives
+#sS+=" -e"
+# PIO       ~~ post-build ~~ (POI_OUT_F) Set flag to create PIO archives
 sS+=" -l"
 #-d                          Deploy the build to github arduino-esp32"
 # ----------------
